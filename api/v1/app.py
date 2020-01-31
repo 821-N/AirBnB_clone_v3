1c392a1a3afc0e06c29560360996f18e9c1214b5
BrandynR <brandyn.reindel@gmail.com>
2020-01-31 05:02:46 +0000

    CORS >:(

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
     app
 """
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -11,6 +12,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
